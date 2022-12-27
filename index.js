1a01615c55d68d6a87d270689734db02755f7251
joharikushagra <skjkushagra1309@gmail.com>
2022-12-27 16:34:14 +0530

    updated needle

--- index.js
+++ index.js
@@ -2,39 +2,35 @@ const cheerio = require("cheerio");
 const needle = require("needle");
 exports.Crawler = Crawler;
 async function Crawler(url) {
-    return new Promise((resolve, reject) => {
-        needle({
-            method: 'GET',
-            url: url
-        }, (err, res, body) => {
-            //console.log(res);
-            var $ = cheerio.load(res.body)
-            var pageUrl = $('head > meta[property="og:url"]').attr('content');
-            var type = $('head > meta[property="og:type"]').attr('content');
-            var image = $('meta[property="og:image"]').attr('content');
-            var imageWidth = $('meta[property="og:image:width"]').attr('content');
-            var imageHeight = $('meta[property="og:image:height"]').attr('content');
-            var description = $('meta[property="og:description"]').attr('content');
-            var heading = $('meta[property="og:title"]').attr('content');
-            var content = $('meta[name="description"]').attr('content');
-            var time = $('meta[property="article:published"]').attr('content');
-            var keywords = $('meta[name="keywords"]').attr('content');
-        
-            var title = {
-                pageUrl,
-                heading,
-                type,
-                image: image,
-                imageWidth,
-                imageHeight,
-                description: description,
-                content,
-                keywords,
-                time: time
-            };
-           
-            resolve(title);
+  return new Promise((resolve, reject) => {
+    needle.get(url, (err, res, body) => {
+      //console.log(res);
+      var $ = cheerio.load(res.body);
+      var pageUrl = $('head > meta[property="og:url"]').attr("content");
+      var type = $('head > meta[property="og:type"]').attr("content");
+      var image = $('meta[property="og:image"]').attr("content");
+      var imageWidth = $('meta[property="og:image:width"]').attr("content");
+      var imageHeight = $('meta[property="og:image:height"]').attr("content");
+      var description = $('meta[property="og:description"]').attr("content");
+      var heading = $('meta[property="og:title"]').attr("content");
+      var content = $('meta[name="description"]').attr("content");
+      var time = $('meta[property="article:published"]').attr("content");
+      var keywords = $('meta[name="keywords"]').attr("content");
 
-        });
-    })
+      var title = {
+        pageUrl,
+        heading,
+        type,
+        image: image,
+        imageWidth,
+        imageHeight,
+        description: description,
+        content,
+        keywords,
+        time: time,
+      };
+
+      resolve(title);
+    });
+  });
 }
